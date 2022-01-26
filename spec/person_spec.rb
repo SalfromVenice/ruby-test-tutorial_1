be78b387d5e757929c83b397acfb40bbb3bc1467
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-26 10:37:59 +0100

    ESlint complaint

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,10 +1,10 @@
-require_relative "../lib/person"
+require_relative '../lib/person'
 
 describe Person do
-  describe "#full_name" do
-    it "returns the first and last names concatenated" do
-      person = Person.new(first_name: "Sal", last_name: "Strangio")
-      expect(person.full_name).to eq "Sal Strangio"
+  describe '#full_name' do
+    it 'returns the first and last names concatenated' do
+      person = Person.new(first_name: 'Sal', last_name: 'Strangio')
+      expect(person.full_name).to eq 'Sal Strangio'
     end
   end
 end
