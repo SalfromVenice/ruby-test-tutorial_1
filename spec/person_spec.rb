da04e839cc6bbfc06a10eb9bb580eedfe355e029
Salvatore Strangio <sal.strangio@gmail.com>
2022-01-26 09:49:35 +0100

    first test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,9 +3,8 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new(first_name: "Sal", last_name: "Strangio")
+      expecting(person.full_name).to eq "Sal Strangio"
     end
   end
 end
